2bb7e1034cf9884ee6275f3518c0afe40d4584a0
Tina Khawase <kdeepashri@icloud.com>
2019-11-20 20:12:57 +0100

    I am Poor App

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Deepashri Khawase on 20.11.19.
+//  Copyright © 2019 Deep Yoga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
